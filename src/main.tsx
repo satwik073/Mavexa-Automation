fb5f190be7008bdd134c07b4ca65b6929935d9c6
satwikWiz <satwik@wizcommerce.com>
2024-09-22 16:41:42 +0530

    feat: Saasy Automation Client

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-// import { ThemeProvider } from './providers/ComplexionsProvider.tsx';
-// import { ThemeProviderOptions } from "./Global/GlobalSiteNavigators/NavigationState/Constants/structure.ts";
+import { ThemeProvider } from './providers/ComplexionsProvider.tsx';
+import { ThemeProviderOptions } from "./Global/GlobalSiteNavigators/NavigationState/Constants/structure.ts";
 import { Provider } from 'react-redux';
 import { store } from "./Store/store.ts";
 
 const container = document.getElementById('root');
 createRoot(container!).render(
-  // <ThemeProvider defaultTheme={ThemeProviderOptions.DARK_TH}>
+  <ThemeProvider defaultTheme={ThemeProviderOptions.DARK_TH}>
     <StrictMode>
         <Provider store={store}>
           <App />
         </Provider>
     </StrictMode>
-  // </ThemeProvider>
+  </ThemeProvider>
 );
